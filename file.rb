075bf17da1b8d35318df503259a1d4c8e14126ae
bish <hamal.bishwash65@gmail.com>
2018-10-18 17:54:43 +0000

    automatically backed up by learn

--- file.rb
+++ file.rb
@@ -1,27 +1,7 @@
-# Create an array of integers and and divide it into odds and evens arrays using .each
+array =["apple", "banana", "grapes," "orange"]
+array1 =  []
+array2= []
+array.each do |element|
 
-array = [1,2,3,4,5,6,7,8,9]
-odds = []
-evens =[]
-
-array.each do |num|
-  if num.even?
-    evens.push(num)
-  else
-    odds.push(num)
-  end
-end
-p evens
-p odds
-
-2.times do |int|
-  int = "hello"
-  puts "it will print twic #{int}"
-end
-
-3.times do |number|
-  number = 16
-  square = number * number
-  puts "The number is #{number} and its square is #{square}."
-  puts "#{square} is square of #{number}."
+  array1.push(element 0,2)
 end
