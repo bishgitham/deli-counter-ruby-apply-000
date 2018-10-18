06a440662d8660ccceec27e408cce19dce2c75f7
bish <hamal.bishwash65@gmail.com>
2018-10-18 17:22:53 +0000

    automatically backed up by learn

--- file.rb
+++ file.rb
@@ -1,21 +1,3 @@
-numbers = [10,20, 30, 7, 40, 45, 81,90, 42 ]
-by10 = []
-by3 = []
-by7 = []
-
-numbers.each do |num|
-  if num %10 == 0
-    by10.push(num)
-  end
-  if num%3 ==0
-      by3.push(num)
-  end
-  if num%7 == 0
-        by7.push(num)
-    end
-end
-
-
 array = [10, 15, 18, 20]
 
 def numbers(array, x)
@@ -31,3 +13,15 @@ end
 
 p numbers(numbers, 5)
 p numbers(numbers, 3)
+
+numbers = [100, 107, 120, 109, 200, 307]
+
+def learning(array,num)
+  array1 =[ ]
+learning.each do |x|
+  if num > x
+    array1.push(num)
+  end
+end
+
+p learning (array1, 100)
