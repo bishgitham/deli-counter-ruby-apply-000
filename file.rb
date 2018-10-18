7bbf646d29481d400ed714cbaf87860737dfe556
bish <hamal.bishwash65@gmail.com>
2018-10-18 14:46:34 +0000

    automatically backed up by learn

--- file.rb
+++ file.rb
@@ -1,5 +1,32 @@
-games = ["football", "cricket", "cards", "ludo"]
-p games.values_at(1, 3)
+numbers = [10,20, 30, 7, 40, 45, 81,90, 42 ]
+by10 = []
+by3 = []
+by7 = []
 
-numbers = [1,3,2,4,5,6,7]
-p numbers[-1]
+numbers.each do |num|
+  if num %10 == 0
+    by10.push(num)
+  end
+  if num%3 ==0
+      by3.push(num)
+  end
+  if num%7 == 0
+        by7.push(num)
+    end
+end
+
+p by10
+p by3
+p by7
+
+
+numbers = [10, 15, 18, 20]
+newarray = []
+secondarray = []
+
+def numbers(num)
+  if num %5 == 0
+    puts newarray.push(num)
+  end
+  
+end
